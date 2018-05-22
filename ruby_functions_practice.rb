3d72a00a53679f88139c84a0fb46ef5cc2efcf79
James Henderson <hendersonjamesalexander@gmail.com>
2018-05-22 16:35:40 +0100

    seven passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,15 @@ def divide(first_number, second_number)
 end
 
 p divide(10, 2)
+
+def length_of_string(test_string)
+  test_string = "a string of length 21"
+  return test_string.length
+end
+
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+join_string("Mary had a little lamb, ", "its fleece was white as snow")
