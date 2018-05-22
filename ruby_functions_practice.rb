214d866c051f8f2d891b4804e39f2df7834cb24a
James Henderson <hendersonjamesalexander@gmail.com>
2018-05-22 16:24:42 +0100

    five tests passed

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,27 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+p add(1, 2)
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+p subtract(10, 5)
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+p multiply(4, 2)
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+p divide(10, 2)
